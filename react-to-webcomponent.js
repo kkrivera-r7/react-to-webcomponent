cd3c91eee6c2f72b007c55db29ce607724a8e936
Kris Rivera <kris_rivera@rapid7.com>
2021-02-19 16:00:25 -0500

    Update for custom operations and ignoring of certain parameters being set

--- react-to-webcomponent.js
+++ react-to-webcomponent.js
@@ -1,24 +1,23 @@
-var reactComponentSymbol = Symbol.for("r2wc.reactComponent");
-var renderSymbol = Symbol.for("r2wc.reactRender");
-var shouldRenderSymbol = Symbol.for("r2wc.shouldRender");
+const reactComponentSymbol = Symbol.for('r2wc.reactComponent');
+const renderSymbol = Symbol.for('r2wc.reactRender');
+const shouldRenderSymbol = Symbol.for('r2wc.shouldRender');
 
-var define = {
-	// Creates a getter/setter that re-renders everytime a property is set.
-	expando: function(receiver, key, value) {
+const define = {
+	// Creates a getter/setter that re-renders every time a property is set.
+	expando: function (receiver, key, value) {
 		Object.defineProperty(receiver, key, {
 			enumerable: true,
-			get: function() {
+			get: function () {
 				return value;
 			},
-			set: function(newValue) {
+			set: function (newValue) {
 				value = newValue;
 				this[renderSymbol]();
-			}
+			},
 		});
 		receiver[renderSymbol]();
-	}
-}
-
+	},
+};
 
 /**
  * Converts a React component into a webcomponent by wrapping it in a Proxy object.
@@ -28,67 +27,81 @@ var define = {
  * @param {Object} options - Optional parameters
  * @param {String?} options.shadow - Use shadow DOM rather than light DOM.
  */
-export default function(ReactComponent, React, ReactDOM, options= {}) {
-	var renderAddedProperties = {isConnected: "isConnected" in HTMLElement.prototype};
-	var rendering = false;
+export default function (ReactComponent, React, ReactDOM, options = {}) {
+	// Warn if there are no propTypes defined
+	if (!ReactComponent.propTypes) {
+		console.warn(`No propTypes found on ${ReactComponent.name} component`);
+	}
+
+	const renderAddedProperties = { isConnected: 'isConnected' in HTMLElement.prototype };
+	let rendering = false;
 	// Create the web component "class"
-	var WebComponent = function() {
-		var self = Reflect.construct(HTMLElement, arguments, this.constructor);
+	const WebComponent = function () {
+		const self = Reflect.construct(HTMLElement, arguments, this.constructor);
 		if (options.shadow) {
 			self.attachShadow({ mode: 'open' });
 		}
 		return self;
 	};
 
-
 	// Make the class extend HTMLElement
-	var targetPrototype = Object.create(HTMLElement.prototype);
+	const targetPrototype = Object.create(HTMLElement.prototype);
 	targetPrototype.constructor = WebComponent;
 
 	// But have that prototype be wrapped in a proxy.
-	var proxyPrototype = new Proxy(targetPrototype, {
+	const proxyPrototype = new Proxy(targetPrototype, {
 		has: function (target, key) {
-			return key in ReactComponent.propTypes ||
-				key in targetPrototype;
+			return key in (ReactComponent.propTypes || {}) || key in targetPrototype;
 		},
 
 		// when any undefined property is set, create a getter/setter that re-renders
-		set: function(target, key, value, receiver) {
-			if (rendering) {
-				renderAddedProperties[key] = true;
+		set: function (target, key, value, receiver) {
+			if (typeof key === 'string' && !(key in proxyPrototype)) {
+				// Do not actually assign if these values
+				if (key === '__ngContext__' || key.startsWith('__zone_symbol')) return true;
+
+				// Prepend 'on' and capitalize first char when we have a dom event
+				if (key.startsWith('_on')) key = `on${key.charAt(3).toUpperCase()}${key.slice(4)}`;
 			}
 
-			if (typeof key === "symbol" || renderAddedProperties[key] || key in target) {
+			if (rendering) renderAddedProperties[key] = true;
+
+			console.log('set prop', { target, key, value, receiver });
+
+			if (typeof key === 'symbol' || renderAddedProperties[key] || key in target) {
 				return Reflect.set(target, key, value, receiver);
 			} else {
-				define.expando(receiver, key, value)
+				define.expando(receiver, key, value);
 			}
 			return true;
 		},
 		// makes sure the property looks writable
-		getOwnPropertyDescriptor: function(target, key){
-			var own = Reflect.getOwnPropertyDescriptor(target, key);
+		getOwnPropertyDescriptor: function (target, key) {
+			const own = Reflect.getOwnPropertyDescriptor(target, key);
 			if (own) {
 				return own;
 			}
 			if (key in ReactComponent.propTypes) {
 				return { configurable: true, enumerable: true, writable: true, value: undefined };
 			}
-		}
+		},
 	});
 	WebComponent.prototype = proxyPrototype;
 
 	// Setup lifecycle methods
-	targetPrototype.connectedCallback = function() {
+	targetPrototype.connectedCallback = function () {
+		const innerHTML = this.innerHTML.trim();
+		if (innerHTML) define.expando(this, 'children', innerHTML);
+
 		// Once connected, it will keep updating the innerHTML.
 		// We could add a render method to allow this as well.
 		this[shouldRenderSymbol] = true;
 		this[renderSymbol]();
 	};
-	targetPrototype[renderSymbol] = function() {
+	targetPrototype[renderSymbol] = function () {
 		if (this[shouldRenderSymbol] === true) {
-			var data = {};
-			Object.keys(this).forEach(function(key) {
+			const data = {};
+			Object.keys(this).forEach(function (key) {
 				if (renderAddedProperties[key] !== false) {
 					data[key] = this[key];
 				}
@@ -105,7 +118,7 @@ export default function(ReactComponent, React, ReactDOM, options= {}) {
 	// Handle attributes changing
 	if (ReactComponent.propTypes) {
 		WebComponent.observedAttributes = Object.keys(ReactComponent.propTypes);
-		targetPrototype.attributeChangedCallback = function(name, oldValue, newValue) {
+		targetPrototype.attributeChangedCallback = function (name, oldValue, newValue) {
 			// TODO: handle type conversion
 			this[name] = newValue;
 		};
